507c3d0cec6f0222ce22bc8964020a583d0356f3
alikashfi <kashfi384@gmail.com>
2023-02-09 14:15:27 +0330

    swagger schemas

---
+++ app/Swagger/CategoryModel.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Swagger;
+
+/**
+ * @OA\Schema(
+ *     title="Category",
+ * )
+ */
+class CategoryModel
+{
+    /**
+     * @OA\Property(
+     *   property="name",
+     *   type="string",
+     *   example="جاوا اسکریپت",
+     *   description=".",
+     * )
+     */
+    public $name;
+
+    /**
+     * @OA\Property(
+     *   property="title",
+     *   type="string",
+     *   description="\<title\> tag for seo",
+     *   example="گروه های برنامه نویسی جاوا اسکریپت",
+     * )
+     */
+    public $title;
+
+    /**
+     * @OA\Property(
+     *   property="slug",
+     *   type="string",
+     *   example="js-gropus",
+     *   description=".",
+     * )
+     */
+    public $slug;
+
+    /**
+     * @OA\Property(
+     *   property="description",
+     *   type="string",
+     *   description="most of times is null. but maybe sometimes needed for seo...",
+     *   example="",
+     * )
+     */
+    public $description;
+
+    /**
+     * @OA\Property(
+     *   property="color",
+     *   type="string",
+     *   example="#ff0000",
+     *   description=".",
+     * )
+     */
+    public $color;
+}
---
+++ app/Swagger/GroupModel.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Swagger;
+
+/**
+ * @OA\Schema(
+ *     title="Group",
+ * )
+ */
+class GroupModel
+{
+    /**
+     * @OA\Property(
+     *   property="name",
+     *   type="string",
+     *   example="گروه برنامه نویسان لاراول در تلگرام",
+     *   description=".",
+     * )
+     */
+    public $name;
+
+    /**
+     * @OA\Property(
+     *   property="slug",
+     *   type="string",
+     *   example="laravel",
+     *   description=".",
+     * )
+     */
+    public $slug;
+
+    /**
+     * @OA\Property(
+     *   property="image",
+     *   type="string",
+     *   example="laravel.jpg",
+     *   description=".",
+     * )
+     */
+    public $image;
+
+    /**
+     * @OA\Property(
+     *   property="description",
+     *   type="string",
+     *   example="",
+     *   description=".",
+     * )
+     */
+    public $description;
+
+    /**
+     * @OA\Property(
+     *   property="address",
+     *   type="string",
+     *   format="uri",
+     *   example="https://t.me/laravel",
+     *   description=".",
+     * )
+     */
+    public $address;
+
+    /**
+     * @OA\Property(
+     *   property="members",
+     *   type="number",
+     *   format="int64",
+     *   example="2000",
+     *   description="number of group members",
+     * )
+     */
+    public $members;
+
+    /**
+     * @OA\Property(
+     *   property="views",
+     *   type="number",
+     *   format="int64",
+     *   example="100",
+     *   description="page views in our site",
+     * )
+     */
+    public $views;
+}
